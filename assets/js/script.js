2e8f3e99cde5df6bb68e13ab7628f860da23f2ed
James Hunter <jamessahunter@gmail.com>
2023-10-06 18:42:00 -0700

    fetch for future weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,10 @@ var futureWeather=$("#future-weather");
 
 button.on("click", function(event){
     event.preventDefault();
-    console.log("works");
-    console.log(city);
+    // console.log("works");
+    // console.log(city);
     var cityInput=city.val();
-    console.log(cityInput);
+    // console.log(cityInput);
     fetchLatLon(cityInput);
     
 
@@ -23,20 +23,21 @@ function fetchLatLon(city){
         if (response.ok){
             response.json().then(function(data){
                 console.log(data);
-                console.log(data[0]);
-                console.log(data[0].lon);
-                console.log(data[0].lat);
+                // console.log(data[0]);
+                // console.log(data[0].lon);
+                // console.log(data[0].lat);
                 var lat=data[0].lat;
                 var lon=data[0].lon;
-                fetchWeather(lat,lon);
+                fetchWeatherCurrent(lat,lon);
+                fetchWeatherFuture(lat,lon);
             })
         }
     })
 
 }
 
-function fetchWeather(lat,lon){
-    var currentUrl="https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=5b9958094719db83e44615746cf27208"
+function fetchWeatherCurrent(lat,lon){
+    var currentUrl="https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=5b9958094719db83e44615746cf27208&units=imperial";
     fetch(currentUrl)
     .then(function(response){
         if (response.ok){
@@ -49,4 +50,19 @@ function fetchWeather(lat,lon){
         }
     })
 
+}
+
+function fetchWeatherFuture(lat,lon){
+    var futureUrl="https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=5b9958094719db83e44615746cf27208&units=imperial";
+    fetch(futureUrl)
+    .then(function(response){
+        if (response.ok){
+            response.json().then(function(data){
+                console.log(data);
+                // console.log(data.wind.speed);
+                // console.log(data.main.temp);
+                // console.log(data.main.humidity);
+            })
+        }
+    })
 }
