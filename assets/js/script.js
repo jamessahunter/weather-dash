67d81cd01e1a2a05e4f15e7ddf1583b1d7ea6a37
James Hunter <jamessahunter@gmail.com>
2023-10-06 18:13:49 -0700

    fetch lat and lon

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,4 +9,29 @@ button.on("click", function(event){
     console.log(city);
     var cityInput=city.val();
     console.log(cityInput);
+    fetchLatLon(cityInput);
+    
+
+
 })
+
+
+function fetchLatLon(city){
+    var cityUrl="http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=5b9958094719db83e44615746cf27208";
+    fetch(cityUrl)
+    .then(function(response){
+        if (response.ok){
+            response.json().then(function(data){
+                console.log(data);
+                console.log(data[0]);
+                console.log(data[0].lon);
+                console.log(data[0].lat);
+                var lat=data[0].lat;
+                var lon=data[0].lon;
+            })
+        }
+    })
+
+
+
+}
